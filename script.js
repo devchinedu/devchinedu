4778f9a240510be7fc665ac4131e2e7b4fe7bed8
devchinedu <63804830+devchinedu@users.noreply.github.com>
2021-05-26 10:55:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.fa-blog').addEventListener('click', function(){
+    document.querySelector('.blog').classList.add('hidden');
+})
+document.querySelector('.fa-address-book').addEventListener('click', function(){
+    document.querySelector('.contact').classList.add('hidden');
+})
