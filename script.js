0e82b57cb7535de656107b6775e4209eefd5f6d1
devchinedu <63804830+devchinedu@users.noreply.github.com>
2021-05-27 17:21:05 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.fa-blog').addEventListener('click', function(){
-    document.querySelector('.blog').classList.add('hidden');
-})
-document.querySelector('.fa-address-book').addEventListener('click', function(){
-    document.querySelector('.contact').classList.add('hidden');
-})
