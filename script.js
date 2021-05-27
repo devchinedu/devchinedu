ae1720bc0298311f897fb6ea88dcb651fd6fc427
devchinedu <63804830+devchinedu@users.noreply.github.com>
2021-05-27 17:23:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const contactus = document.querySelector('.contactus');
+const contactpage = document.querySelector('.contactPage');
+const sendbtn = document.querySelector('#sendmessage');
+const sent = document.querySelector('#thankyou');
+const closebtn = document.getElementsByClassName('close');
+
+
+// event.preventDefault();
+
+contactus.addEventListener('click', function(){
+    contactpage.classList.add('hidden');
+});
+
+
+sendbtn.addEventListener('click', function(){
+    sent.style.display = 'flex';
+    contactpage.classList.remove('hidden');
+    
+});
+
+
+for(var i = 0 ; i < closebtn.length; i++){
+closebtn[i].addEventListener('click', function(){
+    contactpage.classList.remove('hidden');
+        sent.style.display = 'none';
+
+})
+}
