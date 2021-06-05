3d2945db11631c7c3d30136b556f4ff1d64703db
devchinedu <c.daviduchenna@gmail.com>
2021-06-05 15:21:24 +0100

    update

--- build/scripts/preloader.js
+++ build/scripts/preloader.js
@@ -1,3 +1,3 @@
-$(window).on("load",function(){
-    $("document").ready($(".loader-wrapper").fadeOut("slow"));
-});
+// $(window).on("load",function(){
+//     $("document").ready($(".loader-wrapper").fadeOut("slow"));
+// });
